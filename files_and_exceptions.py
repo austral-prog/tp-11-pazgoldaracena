a31ac99c67a156c34628f668bfba475ff2b27907
Pachu Goldaracena <pachugoldaracena@Pachus-MacBook-Pro.local>
2025-07-10 15:18:46 -0300

    updated file

--- files_and_exceptions.py
+++ files_and_exceptions.py
@@ -5,7 +5,24 @@ def read_file_to_dict(filename):
     :return: dict - diccionario con listas de montos por producto.
     :raises: FileNotFoundError - si el archivo no existe.
     """
-    return {}
+    try:
+        with open(filename, "r") as file:
+            reader = file.read()
+            pairs_string = reader.split(";")
+            pairs = []
+            for string in pairs_string:
+                if ":" in string:
+                    key, value = string.split(":")
+                    pairs.append((key, value))
+            data_dict = {}
+            for key, value in pairs:
+                if key not in data_dict:
+                    data_dict[key] = [round(float(value), 1)]
+                else:
+                    data_dict[key].append(round(float(value), 1))
+        return data_dict
+    except FileNotFoundError:
+        raise FileNotFoundError("El archivo no existe")
 
 
 def process_dict(data):
@@ -14,4 +31,7 @@ def process_dict(data):
     :param data: dict - diccionario a procesar.
     :return: None
     """
-    pass
+    for producto, precios in data.items():
+        prod_sum = sum(precios)
+        prod_avg = prod_sum / len(precios)
+        print(f"{producto}: ventas totales ${prod_sum:.2f}, promedio ${prod_avg:.2f}")
